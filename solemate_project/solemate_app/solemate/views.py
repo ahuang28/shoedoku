fe6d7e428b17ef79afebfb9af90bb4c82fdb525e
ahuang28 <amyhuang0605@gmail.com>
2023-09-30 17:15:14 -0400

    added the pages

--- solemate_project/solemate_app/solemate/views.py
+++ solemate_project/solemate_app/solemate/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the solemate index.")
+    template = loader.get_template("solemate/index.html")
+    return HttpResponse(template.render(request=request))
 
 def results(request):
-    return HttpResponse("Hello, world. You're at the solemate results.")
+    template = loader.get_template("solemate/results.html")
+    return HttpResponse(template.render(request=request))
