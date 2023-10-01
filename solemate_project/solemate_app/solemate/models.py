78a82afc0d020062f936730f7c069f43920da82b
William Liu Man Hin <69032106+NxRd01@users.noreply.github.com>
2023-09-30 22:27:46 -0400

    Sneaker object change+ utils
    Changed the sneaker object to have only id and path
    Added many utils to get paths and next images
    added basic views for buttons and more

--- solemate_project/solemate_app/solemate/models.py
+++ solemate_project/solemate_app/solemate/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from solemate.utils import cid_to_path
 # i solemnly swear i will not cut alan off
 
 # all strings
@@ -14,24 +14,15 @@ from django.db import models
 # ToeStyle
 
 class Sneaker(models.Model):
-    cid = models.CharField(max_length=20, primary_key=True)
-    category = models.CharField(max_length=50)
-    subcategory = models.CharField(max_length=50)
-    heel_height = models.CharField(max_length=50)
-    insole = models.CharField(max_length=50)
-    closure = models.CharField(max_length=50)
-    gender = models.CharField(max_length=10)
-    material = models.CharField(max_length=50)
-    toestyle = models.CharField(max_length=50)
-    brand = models.CharField(max_length=50)
-    name = models.CharField(max_length=100)
-    color = models.CharField(max_length=50)
-    price = models.CharField(max_length=10)
-    url = models.CharField(max_length=200)
-    
+    cid = models.CharField(max_length=50, primary_key=True)
+    path = models.CharField(max_length=100, blank=True)  # Adjust the max_length as needed
+
+    def save(self, *args, **kwargs):
+        # Calculate the path based on cid using the cid_to_path function
+        self.path = cid_to_path(str(self.cid))
+        super(Sneaker, self).save(*args, **kwargs)
     
 
     def __str__(self):
-        return f"CID: {self.cid}, Category: {self.category}, Subcategory: {self.subcategory}, Heel/Height: {self.heel_height}, Insole: {self.insole}, Closure: {self.closure}, Gender: {self.gender}, Material:{self.material}, ToeStyle: {self.toestyle}, \
-Brand: {self.brand}, Name: {self.name}, Color: {self.color}, Price: {self.price}, URL: {self.url}"
+        return f"CID: {self.cid}"
 
